989966b09752047d9ff9f236bc3270eecc0bd361
bangdal <ubg11111@naver.com>
2022-04-18 22:23:45 +0900

    자바스크립트 반복문

--- javascript/tab.js
+++ javascript/tab.js
@@ -63,11 +63,23 @@ price.innerHTML = car2.price[0];
         // 현재 메서드안에서 선언된 this는 해당 객체의 메서드를 가리킨다.
         // let value = this.value;
 
+        let pants = [28, 30, 32, 34];
+        let shirts = [90, 95, 100, 105];
+
         // e(이벤트함수) currentTarget은 현재 이벤트리스너가 달린요소
         let value = e.currentTarget.value;
 
         if(value == '셔츠'){
+            
             document.querySelectorAll('.form-select')[1].classList.remove('form-hide');
+            $('.form-select').eq(1).html('');
+
+            for(let i=0; i<pants.length; i++){
+
+                $('.form-select').eq(1).append(`<option>${shirts[i]}</option>`)
+
+            }
+
         }else if(value == '모자'){
             document.querySelectorAll('.form-select')[1].classList.add('form-hide');
         }else if(value == '바지'){
@@ -76,14 +88,26 @@ price.innerHTML = car2.price[0];
             
             // '' or "" 는 엔터형식으로 연결해서 작성이 불가능하기에 ``(백틱) 형식으로 작성하는것이 옳다.
             // 셀렉트 추가하기
-            let templet = `<option>28</option>
-                            <option>30</option>`;
 
-            $('.form-select').eq(1).append(templet);
+            // 배열에만 붙일 수 있는 반복문 forEach() 메서드
+            pants.forEach((item) => { // 화살표함수의 사용주의는 this문을 사용하게되면 지역변수가아닌 전역변수를 참고하게된다.
+               
+                // 콜백함수(e)는 pants 반복문의 변수값을 말한다 
+                // e를 출력하는경우 pants안에 있는 배열의값을 또는 변수값을 모두 출력할 수 있다.
+                $('.form-select').eq(1).append(`<option>${item}</option>`);
+            });
         }
-
-    })
+    });
     
+    // Object 자료 갯수만큼 반복문으로 출력해보기
+    let obj = {name : "you", age : 30};
+
+    for(let key in obj){
+
+        console.log(obj[key]);
+
+    }
+
 
 // 자바스크립트로 html을 생성하는 방법
 
